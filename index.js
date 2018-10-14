e6ac029f4d60a06d3cf3fa50207d51cf210dd459
Roger Mori <moritues@unlv.nevada.edu>
2018-10-13 23:11:35 -0700

    Added scape and finished SOQL and SOSL

---
+++ index.js
@@ -0,0 +1,36 @@
+const isEmpty = require('objects-made-easy').isEmpty;
+const stringify = require('query-string').stringify;
+const axios = require('axios');
+
+const defaultAPIVersion = 'v43.0';
+
+module.exports.easySalesforceAPI = (orgURL, version) => {
+  if (isEmpty(version)) version = defaultAPIVersion;
+  if (isEmpty(orgURL)) {
+      throw new Error('Organization URL must be set prior to usage');
+  }
+
+  const apiURL=`${orgURL}/services/data/${version}`;
+
+  return (token) => {
+      if (isEmpty(token)) {
+          throw new Error('token must be set prior to usage');
+      }
+      const headers= {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`
+      };
+      return {
+          escapeSOQL: (str) => str.replace(/(['\\])/g, '\\$&'),
+          escapeSOSL: (str) => str.replace(/([?&|!{}[\]()^~*:\\"'+-])/g, '\\$&'),
+          SOQL: (query)=>{
+              const url = `${apiURL}/query/?`+stringify({q:query});
+              return axios.get(url,{headers});
+          },
+          SOSL: (query)=>{
+              const url = `${apiURL}/search/?`+stringify({q:query});
+              return axios.get(url,{headers});
+          }
+      }
+  }
+};
